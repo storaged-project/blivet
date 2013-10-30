663fdf51fe9be545288a46e75447a7d72ffaa46c
Brian C. Lane <bcl@redhat.com>
2013-10-30 16:57:21 -0700

    Set the correct version numbers

--- blivet/__init__.py
+++ blivet/__init__.py
@@ -20,7 +20,7 @@
 # Red Hat Author(s): Dave Lehman <dlehman@redhat.com>
 #
 
-__version__ = '0.18.7'
+__version__ = '0.18.8'
 
 ##
 ## Default stub values for installer-specific stuff that gets set up in
