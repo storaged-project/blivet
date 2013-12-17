1ff04fc0e33029627f0957fd80e0efba96f1b334
mulhern <amulhern@redhat.com>
2013-12-17 11:03:34 -0500

    Changes to the handling of filesystem labeling (#1038590)
    Resolves: rhbz#1038590
    
    * Change FS.label to a property
    - Want to guard setting the label format, so it matches the requirements of
    the labelfs program that will use it.
    - Don't set label property in writeLabel. writeLabel is only called from
    doFormat, in a context where self.label is guaranteed to be set.
    * Removed _defaultLabelOptions field and defaultLabelOptions property. The
    property is only used in this file, and it is only used by _getLabelArgs.
    The property is set from the field.
    * Remove label parameter from writeLabel method
    - writeLabel is only called in one place and in that place self.label is
    guaranteed to be set, so this change does not change the behavior of the
    program.
    - Since writeLabel uses the label in self.label instead of a parameter
    it is guaranteed that this value has passed the checks in self._setLabel.
    - Other methods that are only called by writeLabel are changed in the same way.
    - writeLabel now throws an exception if it can not set the label because there
    is no program to set it. This does not change the behavior of this program.
    * Factor out filesystem relabeling functionality into a separate package
    * Add the ability to read the filesystem label
    - In some cases, the application that is used to write the label is also
    used to read the label.
    - This is certainly useful for testing, and may come in handy otherwise.
    
    Signed-off-by: mulhern <amulhern@redhat.com>
    
    Conflicts:
    	blivet/formats/fs.py

--- tests/formats_test/misc_test.py
+++ tests/formats_test/misc_test.py
@@ -4,6 +4,60 @@ import unittest
 from blivet.formats import device_formats
 import blivet.formats.fs as fs
 
+class InitializationTestCase(unittest.TestCase):
+    """Test FS object initialization."""
+
+    def testLabels(self):
+        """Initialize some filesystems with valid and invalid labels."""
+
+        # Ext2FS has a maximum length of 16
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.Ext2FS,
+           device="/dev", label="root___filesystem")
+
+        self.assertIsNotNone(fs.Ext2FS(label="root__filesystem"))
+
+        # FATFS has a maximum length of 11
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.FATFS,
+           device="/dev", label="rtfilesystem")
+
+        self.assertIsNotNone(fs.FATFS(label="rfilesystem"))
+
+        # JFS has a maximum length of 16
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.JFS,
+           device="/dev", label="root___filesystem")
+
+        self.assertIsNotNone(fs.JFS(label="root__filesystem"))
+
+        # ReiserFS has a maximum length of 16
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.ReiserFS,
+           device="/dev", label="root___filesystem")
+
+        self.assertIsNotNone(fs.ReiserFS(label="root__filesystem"))
+
+        #XFS has a maximum length 12 and does not allow spaces
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.XFS,
+           device="/dev", label="root filesyst")
+        self.assertRaisesRegexp(fs.FSError,
+           "Filesystem label.*incorrectly formatted",
+           fs.XFS,
+           device="/dev", label="root file")
+
+        self.assertIsNotNone(fs.XFS(label="root_filesys"))
+
+        # all devices are permitted to have a label of None
+        for k, v  in device_formats.items():
+            self.assertIsNotNone(v(label=None))
+
 class MethodsTestCase(unittest.TestCase):
     """Test some methods that do not require actual images."""
 
@@ -11,36 +65,36 @@ class MethodsTestCase(unittest.TestCase):
         self.fs = {}
         for k, v  in device_formats.items():
             if issubclass(v, fs.FS) and not issubclass(v, fs.NFS):
-                self.fs[k] = v(device="/dev")
+                self.fs[k] = v(device="/dev", label="myfs")
 
 
     def testGetLabelArgs(self):
         self.longMessage = True
 
-        # ReiserFS is currently backwards, needs the label after the l flag
-        for k, v in [(k, v) for k, v in self.fs.items() if isinstance(v, fs.ReiserFS)]:
-            self.assertEqual(v._getLabelArgs("myfs"), ["-l", "/dev", "myfs"], msg=k)
+        # ReiserFS uses a -l flag
+        reiserfs = self.fs["reiserfs"]
+        self.assertEqual(reiserfs._labelfs.setLabelCommand(reiserfs),
+           ["reiserfstune", "-l", "myfs", "/dev"], msg="reiserfs")
 
-        # JFS is backward as well
-        for k, v in [(k, v) for k, v in self.fs.items() if isinstance(v, fs.JFS)]:
-            self.assertEqual(v._getLabelArgs("myfs"), ["-L", "/dev", "myfs"], msg=k)
+        # JFS, XFS use a -L flag
+        lflag_classes = [fs.JFS, fs.XFS]
+        for k, v in [(k, v) for k, v in self.fs.items() if any(isinstance(v, c) for c in lflag_classes)]:
+            self.assertEqual(v._labelfs.setLabelCommand(v), [v._labelfs.name, "-L", "myfs", "/dev"], msg=k)
 
-        #XFS uses a -L label
-        for k, v in [(k, v) for k, v in self.fs.items() if isinstance(v, fs.XFS)]:
-            self.assertEqual(v._getLabelArgs("myfs"), ["-L", "myfs", "/dev"], msg=k)
+        # Ext2FS and descendants and FATFS do not use a flag
+        noflag_classes = [fs.Ext2FS, fs.FATFS]
+        for k, v in [(k, v) for k, v in self.fs.items() if any(isinstance(v, c) for c in noflag_classes)]:
+            self.assertEqual(v._labelfs.setLabelCommand(v), [v._labelfs.name, "/dev", "myfs"], msg=k)
 
-
-        # All NoDeviceFSs ignore the device argument passed and set device
-        # to the fs type
-        for k, v in [(k, v) for k, v in self.fs.items() if isinstance(v, fs.NoDevFS)]:
-            self.assertEqual(v._getLabelArgs("myfs"), [v.type, "myfs"], msg=k)
-
-        for k, v in [(k, v) for k, v in self.fs.items() if not (isinstance(v, fs.NoDevFS) or isinstance(v, fs.ReiserFS) or isinstance(v, fs.XFS) or isinstance(v, fs.JFS))]:
-            self.assertEqual(v._getLabelArgs("myfs"), ["/dev", "myfs"], msg=k)
+        # all of the remaining should have no labelfsProg
+        omit_classes = [ fs.ReiserFS ] + lflag_classes + noflag_classes
+        for k, v in [(k, v) for k, v in self.fs.items() if not any(isinstance(v, c) for c in omit_classes)]:
+            self.assertIsNone(v.labelfsProg)
 
 def suite():
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(MethodsTestCase)
-    return unittest.TestSuite(suite1)
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(InitializationTestCase)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(MethodsTestCase)
+    return unittest.TestSuite([suite1, suite2])
 
 
 if __name__ == "__main__":
