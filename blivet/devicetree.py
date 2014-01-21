storaged-project/blivet

commit a11f3a8795edb6f28211e9b05aa8df65c2d91e13

Multipath, fwraid members need not be in exclusiveDisks. (#1032919)
This, combined with commit 07bcfa413f5e4d1fdb8ebcb9ac98834da29f0df2,
means you can specify a multipath by identifying the multipath itself,
all of its members, or both.

blivet/devicetree.py
@@ -1135,6 +1135,15 @@ class DeviceTree(object):
                 for parent in device.parents:
                     parent.protected = True
 
+        # If we just added a multipath or fwraid disk that is in exclusiveDisks
+        # we have to make sure all of its members are in the list too.
+        mdclasses = (DMRaidArrayDevice, MDRaidArrayDevice, MultipathDevice)
+        if device.isDisk and isinstance(device, mdclasses):
+            if device.name in self.exclusiveDisks:
+                for parent in device.parents:
+                    if parent.name not in self.exclusiveDisks:
+                        self.exclusiveDisks.append(parent.name)
+
         # Don't try to do format handling on drives without media or
         # if we didn't end up with a device somehow.
         if not device or not device.mediaPresent:

commit caf8d3f984d7630cd2b34a934fba9652b4d6a54f

Fix potential traceback in devicetree.populate. (#1055523)
Traceback introduced by commit a11f3a8795ed from master.

blivet/devicetree.py
@@ -1138,7 +1138,7 @@ class DeviceTree(object):
         # If we just added a multipath or fwraid disk that is in exclusiveDisks
         # we have to make sure all of its members are in the list too.
         mdclasses = (DMRaidArrayDevice, MDRaidArrayDevice, MultipathDevice)
-        if device.isDisk and isinstance(device, mdclasses):
+        if device and device.isDisk and isinstance(device, mdclasses):
             if device.name in self.exclusiveDisks:
                 for parent in device.parents:
                     if parent.name not in self.exclusiveDisks: