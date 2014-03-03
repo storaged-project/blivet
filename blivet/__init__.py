6ddcdc45909f8913dcccb009d7bd28fdba57c20e
mulhern <amulhern@redhat.com>
2014-03-03 12:33:03 -0500

    When copying a root, also copy hidden devices (#1043763)
    Related: rhbz#1043763
    
    Otherwise, even if the devices are unhidden the roots mounts or swaps will
    remain empty.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- blivet/__init__.py
+++ blivet/__init__.py
@@ -1989,18 +1989,15 @@ class Blivet(object):
             p = partition.disk.format.partedDisk.getPartitionByPath(partition.path)
             partition.partedPartition = p
 
-        # make sure the roots reference devices in self.devicetree._devices
         for root in new.roots:
-            root.swaps = [new.devicetree.getDeviceByID(d.id) for d in root.swaps]
-            # the above will put None in the list if the swap has since been
-            # removed, which is problematic
+            root.swaps = [new.devicetree.getDeviceByID(d.id, hidden=True) for d in root.swaps]
             root.swaps = [s for s in root.swaps if s]
 
             for (mountpoint, old_dev) in root.mounts.items():
                 if old_dev is None:
                     continue
 
-                new_dev = new.devicetree.getDeviceByID(old_dev.id)
+                new_dev = new.devicetree.getDeviceByID(old_dev.id, hidden=True)
                 if new_dev is None:
                     # if the device has been removed don't include this
                     # mountpoint at all
