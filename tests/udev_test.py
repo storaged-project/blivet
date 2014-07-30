1e60c603321729c71895ac5dc19adc669cce4a72
David Lehman <dlehman@redhat.com>
2014-07-30 17:12:58 -0500

    Clean up mocking done by udev tests when finished.

--- tests/udev_test.py
+++ tests/udev_test.py
@@ -7,8 +7,18 @@ class UdevTest(unittest.TestCase):
 
     def setUp(self):
         import blivet.udev
+        self._blivet_os = blivet.udev.os
+        self._blivet_log = blivet.udev.log
+        self._blivet_util = blivet.udev.util
         blivet.udev.os = mock.Mock()
         blivet.udev.log = mock.Mock()
+        blivet.udev.util = mock.Mock()
+
+    def tearDown(self):
+        import blivet.udev
+        blivet.udev.log = self._blivet_log
+        blivet.udev.os = self._blivet_os
+        blivet.udev.util = self._blivet_util
 
     def test_udev_get_device(self):
         import blivet.udev
@@ -18,13 +28,11 @@ class UdevTest(unittest.TestCase):
 
     def udev_settle_test(self):
         import blivet.udev
-        blivet.udev.util = mock.Mock()
         blivet.udev.settle()
         self.assertTrue(blivet.udev.util.run_program.called)
 
     def udev_trigger_test(self):
         import blivet.udev
-        blivet.udev.util = mock.Mock()
         blivet.udev.trigger()
         self.assertTrue(blivet.udev.util.run_program.called)
 
