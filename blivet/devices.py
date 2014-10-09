1aede5f41cf7cef86ed0b9a0942f47d0288f6fa7
David Shea <dshea@redhat.com>
2014-10-09 16:56:55 -0400

    Don't append btrfs mount options to None (#1150872)
    (cherry picked from commit 09ae534eecc4f9eb2fb9fcb692630f438b930ee8)
    
    (sort of. change applied to blivet/devices.py instead of
    blivet/devices/btrfs.py)

--- blivet/devices.py
+++ blivet/devices.py
@@ -5410,7 +5410,10 @@ class BTRFSSubVolumeDevice(BTRFSDevice):
             return
 
         # propagate mount options specified for members via kickstart
-        opts = "%s,subvol=%s" % (self.volume.format.mountopts, self.name)
+        opts = "subvol=%s" % self.name
+        if self.volume.format.mountopts:
+            opts = "%s,%s" % (self.volume.format.mountopts, opts)
+
         self.format.mountopts = opts
 
     @property
