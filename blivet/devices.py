d3f284b1c22b2c89c41b0b79fd9f275678d32498
mulhern <amulhern@redhat.com>
2014-10-17 12:45:59 -0400

    Set sysfsPath attribute before calling Device constructor (#1150147)
    Device constructor sets parents, but MDRaidArrayDevice's _addParent()
    method requires that sysfsPath attribute be present.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- blivet/devices.py
+++ blivet/devices.py
@@ -567,13 +567,17 @@ class StorageDevice(Device):
 
         self.exists = exists
         self.uuid = uuid
+
+        # Set sysfsPath before super call as MDRaidArrayDevice._addParent()
+        # reads sysfsPath.
+        self.sysfsPath = sysfsPath
+
         Device.__init__(self, name, parents=parents)
 
         self._format = None
         self._size = Size(util.numeric_type(size))
         self.major = util.numeric_type(major)
         self.minor = util.numeric_type(minor)
-        self.sysfsPath = sysfsPath
         self._serial = serial
         self._vendor = vendor
         self._model = model
