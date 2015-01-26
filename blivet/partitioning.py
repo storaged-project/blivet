storaged-project/blivet

commit dcfd06a7cdb7b9b2884664435fd775b20b11ccda

Don't crash when a free region is too small for an aligned partition.
Resolves: rhbz#1167292

blivet/partitioning.py
@@ -1122,12 +1122,18 @@ def allocatePartitions(storage, disks, partitions, freespace):
 
                                     continue
 
-                            temp_part = addPartition(disklabel,
-                                                     _free,
-                                                     _part_type,
-                                                     _part.req_size,
-                                                     _part.req_start_sector,
-                                                     _part.req_end_sector)
+                            try:
+                                temp_part = addPartition(disklabel,
+                                                         _free,
+                                                         _part_type,
+                                                         _part.req_size,
+                                                         _part.req_start_sector,
+                                                         _part.req_end_sector)
+                            except ArithmeticError as e:
+                                log.debug("failed to allocate aligned partition "
+                                         "for growth test")
+                                continue
+
                             _part.partedPartition = temp_part
                             _part.disk = _disk
                             temp_parts.append(_part)

commit 8d2ef8ac021c8a52aeeda3f742d484fd212b1379

More fixes for alignment-related partition allocation failures.
Commit dcfd06a7cdb7b9 caused an UnboundLocalError, which this fixes.

Also, handle alignment-related failures when no growable requests are
being allocated.

Related: rhbz#1167292

blivet/partitioning.py
@@ -1122,6 +1122,7 @@ def allocatePartitions(storage, disks, partitions, freespace):
 
                                     continue
 
+                            temp_part = None
                             try:
                                 temp_part = addPartition(disklabel,
                                                          _free,
@@ -1164,7 +1165,8 @@ def allocatePartitions(storage, disks, partitions, freespace):
                                         sectorsToSize(disk_growth,
                                                       disk_sector_size))
 
-                    disklabel.partedDisk.removePartition(temp_part)
+                    if temp_part:
+                        disklabel.partedDisk.removePartition(temp_part)
                     _part.partedPartition = None
                     _part.disk = None
 
@@ -1230,8 +1232,12 @@ def allocatePartitions(storage, disks, partitions, freespace):
                 raise PartitioningError(_("not enough free space after "
                                         "creating extended partition"))
 
-        partition = addPartition(disklabel, free, part_type, _part.req_size,
-                                 _part.req_start_sector, _part.req_end_sector)
+        try:
+            partition = addPartition(disklabel, free, part_type, _part.req_size,
+                                _part.req_start_sector, _part.req_end_sector)
+        except ArithmeticError:
+            raise PartitioningError(_("failed to allocate aligned partition"))
+
         log.debug("created partition %s of %s and added it to %s",
                 partition.getDeviceNodeName(),
                 Size(partition.getLength(unit="B")),