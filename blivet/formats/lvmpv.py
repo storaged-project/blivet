1891587f6a4369165eeae785755aae772b946716
mulhern <amulhern@redhat.com>
2015-03-12 16:23:36 -0400

    Add availability information in formats that use blockdev.
    Related: #12
    
    And define formattable and supported methods.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- blivet/formats/lvmpv.py
+++ blivet/formats/lvmpv.py
@@ -27,6 +27,7 @@ from gi.repository import GLib
 from ..storage_log import log_method_call
 from parted import PARTITION_LVM
 from ..devicelibs import lvm
+from ..tasks import availability
 from ..i18n import N_
 from ..size import Size
 from . import DeviceFormat, register_device_format
@@ -47,6 +48,7 @@ class LVMPhysicalVolume(DeviceFormat):
     _minSize = lvm.LVM_PE_SIZE * 2      # one for metadata and one for data
     _packages = ["lvm2"]                # required packages
     _ksMountpoint = "pv."
+    _plugin = availability.BLOCKDEV_LVM_PLUGIN
 
     def __init__(self, **kwargs):
         """
@@ -94,6 +96,14 @@ class LVMPhysicalVolume(DeviceFormat):
         d.update({"vgName": self.vgName, "vgUUID": self.vgUuid,
                   "peStart": self.peStart, "dataAlignment": self.dataAlignment})
         return d
+
+    @property
+    def formattable(self):
+        return super(LVMPhysicalVolume, self).formattable and self._plugin.available
+
+    @property
+    def supported(self):
+        return super(LVMPhysicalVolume, self).supported and self._plugin.available
 
     def _create(self, **kwargs):
         log_method_call(self, device=self.device,
