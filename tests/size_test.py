storaged-project/blivet

commit 65703bfc851f6d919f8e66e1a635ace5835ea6c1

Add ability to match scientific notation in strings.
The matching regular expression is expanded to include possible
scientific notation in numeric part and to exclude numeric parts
with multiple radices.

Uses named groups instead of indices.

Signed-off-by: mulhern <amulhern@redhat.com>

blivet/size.py
@@ -173,19 +173,16 @@ def _parseSpec(spec):
     if radix != '.':
         spec = spec.replace(radix, '.')
 
-    # Match the string using only digit/space/not-space, since the
-    # string might be non-English and contain non-letter characters
-    # that Python doesn't understand as parts of words.
-    m = re.match(r'(-?\s*[0-9.]+)\s*([^\s]*)$', spec.strip())
+    m = re.match(r'(?P<numeric>(-|\+)?\s*(?P<base>([0-9]+)|([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*))(?P<exp>(e|E)(-|\+)[0-9]+)?)\s*(?P<rest>[^\s]*)$', spec.strip())
     if not m:
         raise ValueError("invalid size specification", spec)
 
     try:
-        size = Decimal(m.groups()[0])
+        size = Decimal(m.group('numeric'))
     except InvalidOperation:
         raise ValueError("invalid size specification", spec)
 
-    specifier = m.groups()[1]
+    specifier = m.group('rest')
 
     # First try to parse as English.
     try:

tests/size_test.py
@@ -184,6 +184,16 @@ class SizeTestCase(unittest.TestCase):
         self.assertEquals(Size("%s KiB" % (1/1025.0,)), Size(0))
         self.assertEquals(Size("%s KiB" % (1/1023.0,)), Size(1))
 
+    def testNoUnitsInString(self):
+        self.assertEqual(Size("1024"), Size("1 KiB"))
+
+    def testScientificNotation(self):
+        self.assertEqual(size._parseSpec("1e+0 KiB"), Decimal(1024))
+        self.assertEqual(size._parseSpec("1e-0 KiB"), Decimal(1024))
+        self.assertEqual(size._parseSpec("1e-1 KB"), Decimal(100))
+        self.assertEqual(size._parseSpec("1E-4KB"), Decimal("0.1"))
+        self.assertEqual(Size("1E-10KB"), Size(0))
+
 class TranslationTestCase(unittest.TestCase):
 
     def __init__(self, methodName='runTest'):

commit aebb1fe6c916a69de935b46d397258a9e44e93cf

Fix a bug in reading a size specification with a radix in the numeric part.
Since the | operator is not greedy, the match would return the first of the
three base alternatives that worked, and would split, e.g., "0.0" into
"0" and ".0" instead of "0.0" and nothing.

Now, base is a single alternative, that should capture numbers with a
decimal point correctly.

Fixes a bug introduced in commit 65703bfc851f6d919f8e66e1a635ace5835ea6c1.

Other changes are:
* No longer allows spaces between optional sign and number. Decimal
constructor will choke on '- 2', so might as well not accept it in regular
expression.
* Uses verbose regular expression, to make it a little easier to read.
Uses separate compile before match call for the same reason.
* No longer allows trailing non-whitespace characters after a separating
space, e.g., "1 KiB or maybe 2". Still  allows trailing or leading
whitespace characters. Never allowed leading non-whitespace characters,
e.g., "nearly 1 PiB".

Other non-changes are:
* Still allows juxtaposition of value and spec, e.g., "1.0e+1KiB".
* Still allows leading 0s in a number, even if they are pointless, e.g., "009".

Adds a few more tests.

Signed-off-by: mulhern <amulhern@redhat.com>

blivet/size.py
@@ -178,7 +178,17 @@ def parseSpec(spec):
     if radix != '.':
         spec = spec.replace(radix, '.')
 
-    m = re.match(r'(?P<numeric>(-|\+)?\s*(?P<base>([0-9]+)|([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*))(?P<exp>(e|E)(-|\+)[0-9]+)?)\s*(?P<rest>[^\s]*)$', spec.strip())
+    spec_re = re.compile(
+       r"""(?P<numeric> # the numeric part consists of three parts, below
+           (-|\+)? # optional sign character
+           (?P<base>([0-9]*(\.[0-9]|[0-9]\.|[0-9])[0-9]*)) # the base
+           (?P<exp>(e|E)(-|\+)[0-9]+)?) # optional exponent
+           \s* # whitespace
+           (?P<rest>[^\s]*$) # the units specification
+        """,
+        re.VERBOSE
+    )
+    m = re.match(spec_re, spec.strip())
     if not m:
         raise ValueError("invalid size specification", spec)
 

tests/size_test.py
@@ -200,6 +200,57 @@ class SizeTestCase(unittest.TestCase):
         self.assertEqual(size.parseSpec("1E-4KB"), Decimal("0.1"))
         self.assertEqual(Size("1E-10KB"), Size(0))
 
+        with self.assertRaises(ValueError):
+            # this is an exponent w/out a base
+            size.parseSpec("e+0")
+
+    def testFloatingPointStr(self):
+        self.assertEqual(size.parseSpec("1.5e+0 KiB"), Decimal(1536))
+        self.assertEqual(size.parseSpec("0.0"), Decimal(0))
+        self.assertEqual(size.parseSpec("0.9 KiB"), Decimal("921.6"))
+        self.assertEqual(size.parseSpec("1.5 KiB"), Decimal(1536))
+        self.assertEqual(size.parseSpec("0.5 KiB"), Decimal(512))
+        self.assertEqual(size.parseSpec(".5 KiB"), Decimal(512))
+        self.assertEqual(size.parseSpec("1. KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec("-1. KiB"), Decimal(-1024))
+        self.assertEqual(size.parseSpec("+1. KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec("+1.0000000e+0 KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec("+.5 KiB"), Decimal(512))
+
+        with self.assertRaises(ValueError):
+            # this is a fragment of an arithmetic expression
+            size.parseSpec("+ 1 KiB")
+
+        with self.assertRaises(ValueError):
+            # this is a fragment of an arithmetic expression
+            size.parseSpec("- 1 KiB")
+
+        with self.assertRaises(ValueError):
+            # this is a lonely .
+            size.parseSpec(". KiB")
+
+        with self.assertRaises(ValueError):
+            # this has a fragmentary exponent
+            size.parseSpec("1.0e+ KiB")
+
+        with self.assertRaises(ValueError):
+            # this is a version string, not a number
+            size.parseSpec("1.0.0")
+
+    def testWhiteSpace(self):
+        self.assertEqual(size.parseSpec("1 KiB "), Decimal(1024))
+        self.assertEqual(size.parseSpec(" 1 KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec(" 1KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec(" 1e+0KiB"), Decimal(1024))
+        with self.assertRaises(ValueError):
+            size.parseSpec("1 KiB just a lot of stray characters")
+        with self.assertRaises(ValueError):
+            size.parseSpec("just 1 KiB")
+
+    def testLeadingZero(self):
+        self.assertEqual(size.parseSpec("001 KiB"), Decimal(1024))
+        self.assertEqual(size.parseSpec("1e+01"), Decimal(10))
+
     def testPickling(self):
         s = Size("10 MiB")
         self.assertEqual(s, cPickle.loads(cPickle.dumps(s)))