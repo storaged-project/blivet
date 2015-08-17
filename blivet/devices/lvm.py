67b04190f4bc02db76a1000cf38348a3e33fcbfe
Vratislav Podzimek <vpodzime@redhat.com>
2015-08-17 16:14:12 +0200

    Fix VG free space check when shrinking an LV
    Shrinking means that there is an LV that fits into the VG (otherwise it cannot
    exist) and thus we can skip the check for maximum size according to the current
    size and VG's free space.

--- blivet/devices/lvm.py
+++ blivet/devices/lvm.py
@@ -616,10 +616,10 @@ class LVMLogicalVolumeDevice(DMDevice):
         log.debug("trying to set lv %s size to %s", self.name, size)
         # Don't refuse to set size if we think there's not enough space in the
         # VG for an existing LV, since it's existence proves there is enough
-        # space for it.
+        # space for it. A similar reasoning applies to shrinking the LV.
         if not self.exists and \
            not isinstance(self, LVMThinLogicalVolumeDevice) and \
-           size > self.vg.freeSpace + self.vgSpaceUsed:
+           size > self.size and size > self.vg.freeSpace + self.vgSpaceUsed:
             log.error("failed to set size: %s short", size - (self.vg.freeSpace + self.vgSpaceUsed))
             raise ValueError("not enough free space in volume group")
 
