7e96f1e9847a4f3b3d4d17e93f38cea6054e9eef
Peter Jones <pjones@redhat.com>
2015-10-14 13:49:37 -0400

    edd: make logging work usefully during tests
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- tests/devicelibs_test/edd_test.py
+++ tests/devicelibs_test/edd_test.py
@@ -2,6 +2,8 @@ import unittest
 import mock
 import os
 import inspect
+import logging
+import copy
 
 from blivet.devicelibs import edd
 
@@ -33,18 +35,56 @@ class FakeEddEntry(edd.EddEntry):
 
 class EddTestCase(unittest.TestCase):
     _edd_logger = None
+    maxDiff = None
+
     def setUp(self):
         super(EddTestCase, self).setUp()
+        if 'WORKSPACE' in os.environ.keys():
+            ws = os.path.join(os.environ['WORKSPACE'], "blivet-edd.log")
+        else:
+            ws = "/tmp/blivet-edd.log"
+        self.handler = logging.FileHandler(ws)
+        self.handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
+        self.handler.setFormatter(formatter)
+        edd.log.addHandler(self.handler)
+
         self._edd_logger = edd.log
-        edd.log = mock.MagicMock(name='log')
-        edd.log.info = mock.MagicMock(name='info')
-        edd.log.warning = mock.MagicMock(name='warning')
-        edd.log.error = mock.MagicMock(name='error')
-        edd.log.debug = mock.MagicMock(name='debug')
+        self._edd_logger_level = edd.log.level
+        edd.log.setLevel(logging.DEBUG)
+        newlog = mock.MagicMock(name='log')
+        newlog.debug = mock.Mock(name='debug',
+                        side_effect=self._edd_logger.debug)
+        newlog.info = mock.Mock(name='info',
+                        side_effect=self._edd_logger.info)
+        newlog.warning = mock.Mock(name='warning',
+                        side_effect=self._edd_logger.warning)
+        newlog.error = mock.Mock(name='error',
+                        side_effect=self._edd_logger.error)
+        edd.log = newlog
 
     def tearDown(self):
-        super(EddTestCase, self).tearDown()
         edd.log = self._edd_logger
+        edd.log.setLevel(self._edd_logger_level)
+        edd.log.removeHandler(self.handler)
+        super(EddTestCase, self).tearDown()
+
+    def checkLogs(self, debugs=None, infos=None, warnings=None, errors=None):
+        def check(left, right_object):
+            left = [mock.call(*x) for x in left or []]
+            left.sort()
+            right = copy.copy(right_object.call_args_list or [])
+            right.sort()
+            self.assertEqual(left, right)
+            if len(left) == 0:
+                self.assertEqual(right_object.called, False)
+            else:
+                self.assertEqual(right_object.called, True)
+
+        check(debugs, edd.log.debug)
+        check(infos, edd.log.info)
+        check(warnings, edd.log.warning)
+        check(errors, edd.log.error)
 
     def _set_fs_root(self, edd_module, fsroot):
         if fsroot is None:
@@ -52,6 +92,13 @@ class EddTestCase(unittest.TestCase):
         else:
             dirname = os.path.dirname(inspect.getfile(edd_module))
             edd_module.fsroot = os.path.join(dirname, "../../tests/devicelibs_test/edd_data/", fsroot)
+
+    def debug(self, *args):
+        fmt = "edd_test: "
+        if len(args) >= 0:
+            fmt += args[0]
+            args = args[1:]
+        self._edd_logger.debug(fmt, *args)
 
     def test_collect_edd_data_sata_usb(self):
         # test with sata sda, usb sdb
@@ -71,13 +118,17 @@ class EddTestCase(unittest.TestCase):
             }
 
         edd_dict = edd.collect_edd_data()
+        self.debug('edd_dict: %s', edd_dict)
+        debugs = [
+            ("edd: found device 0x%x at %s", 0x80,
+                    '/sys/firmware/edd/int13_dev80'),
+            ("edd: found device 0x%x at %s", 0x81,
+                    '/sys/firmware/edd/int13_dev81'),
+            ]
         self.assertEqual(len(edd_dict), 2)
         self.assertEqual(fakeedd[0x80], edd_dict[0x80])
         self.assertEqual(fakeedd[0x81], edd_dict[0x81])
-        self.assertEqual(edd.log.debug.called, False)
-        self.assertEqual(edd.log.info.called, False)
-        self.assertEqual(edd.log.warning.called, False)
-        self.assertEqual(edd.log.error.called, False)
+        self.checkLogs(debugs=debugs)
 
     def test_get_edd_dict_sata_usb(self):
         # test with sata sda, usb sdb
@@ -102,12 +153,17 @@ class EddTestCase(unittest.TestCase):
             }
 
         edd_dict = edd.get_edd_dict(devices)
+        self.debug('edd_dict: %s', edd_dict)
         self.assertEqual(len(edd_dict), 2)
         self.assertEqual(edd_dict["sda"], 0x80)
         self.assertEqual(edd_dict["sdb"], 0x81)
         debugs = [
             ("edd: data extracted from 0x%x:\n%s", 0x80, fakeedd[0x80]),
             ("edd: data extracted from 0x%x:\n%s", 0x81, fakeedd[0x81]),
+            ("edd: found device 0x%x at %s", 0x80,
+                    '/sys/firmware/edd/int13_dev80'),
+            ("edd: found device 0x%x at %s", 0x81,
+                    '/sys/firmware/edd/int13_dev81'),
             ]
         infos = [
             ("edd: MBR signature on %s is zero. new disk image?", "sda"),
@@ -120,10 +176,4 @@ class EddTestCase(unittest.TestCase):
                 "/sys/firmware/edd/int13_dev81"),
             ("edd: interface details: %s", "USB     \tserial_number: 30302e31"),
             ]
-        for debug in debugs:
-            self.assertIn(mock.call(*debug), edd.log.debug.call_args_list)
-        for info in infos:
-            self.assertIn(mock.call(*info), edd.log.info.call_args_list)
-        for warning in warnings:
-            self.assertIn(mock.call(*warning), edd.log.warning.call_args_list)
-        self.assertEqual(edd.log.error.called, False)
+        self.checkLogs(debugs=debugs, infos=infos, warnings=warnings)
