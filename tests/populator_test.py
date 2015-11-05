f5ba22f335092fee50c1f97008d0e4eb11cdb586
David Lehman <dlehman@redhat.com>
2015-11-05 11:04:00 -0500

    Fix missed rebase of populator test from 169dc6b6.

--- tests/populator_test.py
+++ tests/populator_test.py
@@ -4,12 +4,13 @@ from blivet import Blivet
 from blivet.flags import flags
 from blivet import util
 from blivet.size import Size
-from blivet.osinstall import storageInitialize
+from blivet.osinstall import storage_initialize
 try:
     from pyanaconda import kickstart
     pyanaconda_present = True
 except ImportError:
     pyanaconda_present = False
+
 
 @unittest.skipUnless(pyanaconda_present, "pyanaconda is missing")
 class setupDiskImagesNonZeroSizeTestCase(unittest.TestCase):
@@ -18,7 +19,7 @@ class setupDiskImagesNonZeroSizeTestCase(unittest.TestCase):
         This test emulates how anaconda configures its storage.
     """
 
-    disks = { "disk1": Size("2 GiB") }
+    disks = {"disk1": Size("2 GiB")}
 
     def setUp(self):
         self.blivet = Blivet()
@@ -26,26 +27,26 @@ class setupDiskImagesNonZeroSizeTestCase(unittest.TestCase):
         # anaconda first configures disk images
         for (name, size) in iter(self.disks.items()):
             path = util.create_sparse_tempfile(name, size)
-            self.blivet.config.diskImages[name] = path
+            self.blivet.config.disk_images[name] = path
 
         # at this point the DMLinearDevice has correct size
-        self.blivet.setupDiskImages()
+        self.blivet.setup_disk_images()
 
         # emulates setting the anaconda flags which later update
-        # blivet flags as the first thing to do in storageInitialize
+        # blivet flags as the first thing to do in storage_initialize
         flags.image_install = True
         # no kickstart available
         ksdata = kickstart.AnacondaKSHandler([])
-        # anaconda calls storageInitialize regardless of whether or not
+        # anaconda calls storage_initialize regardless of whether or not
         # this is an image install. Somewhere along the line this will
-        # execute setupDiskImages() once more and the DMLinearDevice created
+        # execute setup_disk_images() once more and the DMLinearDevice created
         # in this second execution has size 0
-        storageInitialize(self.blivet, ksdata, [])
+        storage_initialize(self.blivet, ksdata, [])
 
     def tearDown(self):
         self.blivet.reset()
-        self.blivet.devicetree.teardownDiskImages()
-        for fn in self.blivet.config.diskImages.values():
+        self.blivet.devicetree.teardown_disk_images()
+        for fn in self.blivet.config.disk_images.values():
             if os.path.exists(fn):
                 os.unlink(fn)
 
