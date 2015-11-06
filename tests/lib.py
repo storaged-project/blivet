94468c5f91f6731acf721f7887ce4a7c0508e992
Peter Jones <pjones@redhat.com>
2015-11-06 15:58:10 -0500

    edd: Make our own assertVerboseListEqual to get rid of truncated output.
    It's really annoying that there's no way to turn off
    unittest.assertEqual()'s stupid string truncation, and it makes it very
    difficult to debug test failures that involve a fairly large amount of
    data.  So replace it with something that tells us the meaningful bits.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ tests/lib.py
@@ -0,0 +1,50 @@
+
+import pprint
+
+def assertVerboseListEqual(left, right, msg=None):
+    left.sort()
+    right.sort()
+
+    found = []
+    rightmissing = []
+    leftmissing = []
+    for x in left:
+        if x in right:
+            found.append(x)
+        else:
+            rightmissing.append(x)
+    for x in right:
+        if x not in found:
+            leftmissing.append(x)
+    if msg:
+        raise AssertionError(msg)
+
+    s = "\n"
+    if leftmissing:
+        s += "log has: \n"
+        for l in leftmissing:
+            s += " %s\n" % (pprint.pformat(l),)
+
+    if rightmissing:
+        s += "expected: \n"
+        for r in rightmissing:
+            s += " %s\n" % (pprint.pformat(r),)
+
+    if leftmissing or rightmissing:
+        raise AssertionError(s)
+
+def assertVerboseEqual(left, right, msg=None):
+    if left != right:
+        l = len(left)
+        r = len(right)
+        for x in range(0, max(l, r)):
+            if x > l-1:
+                assertVerboseEqual(None, right[x], msg)
+            if x > r-1:
+                assertVerboseEqual(left[x], None, msg)
+            if left[x] != right[x]:
+                if msg:
+                    raise AssertionError(msg)
+                else:
+                    raise AssertionError("%s != %s" % (
+                            pprint.pformat(left), pprint.pformat(right)))
