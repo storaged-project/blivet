4bd93ca377c11ef92dfaed701e494938402b0e03
Vratislav Podzimek <vpodzime@redhat.com>
2016-06-23 10:56:13 +0200

    Cache and reuse data about multipath members
    With the new function provided by libblockdev we can easily get the list of
    multipath member devices once and then query it instead of querying every device
    whether it is an mpath member or not.

--- blivet/events/handler.py
+++ blivet/events/handler.py
@@ -24,6 +24,7 @@ from ..errors import DeviceError, EventHandlingError
 from ..devices import DM_MAJORS, MD_MAJORS
 from .. import udev
 from ..threads import SynchronizedMeta
+from ..static_data import mpath_members
 
 from .changes import data as event_data
 from .changes import record_change
@@ -129,6 +130,7 @@ class EventHandlerMixin(metaclass=SynchronizedMeta):
         # XXX Don't change anything (except simple attributes?) if actions are being executed.
         if device is None and self._event_device_is_physical_disk(event):
             log.info("disk %s was added", udev.device_get_name(event.info))
+            mpath_members.update_cache(udev.device_get_devname(event.info))
             self.handle_device(event.info)
         elif device is not None and device.exists:
             log.info("device %s was activated", device.name)
