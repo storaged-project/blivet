47cf097244bceb48bcb62949843b6652881a48a4
David Lehman <dlehman@redhat.com>
2017-11-08 22:44:39 -0500

    New version: 3.0.0.b1

--- blivet/__init__.py
+++ blivet/__init__.py
@@ -20,7 +20,7 @@
 #                    Vratislav Podzimek <vpodzime@redhat.com>
 #
 
-__version__ = '2.1.11'
+__version__ = '3.0.0.b1'
 
 import sys
 import importlib
--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ data_files = [
 
 
 setup(name='blivet',
-      version='2.1.11',
+      version='3.0.0.b1',
       cmdclass={"sdist": blivet_sdist},
       description='Python module for system storage configuration',
       author='David Lehman', author_email='dlehman@redhat.com',
