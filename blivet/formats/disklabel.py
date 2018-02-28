storaged-project/blivet

commit ebd43b67aaeae8bd969c5ef9ba7280a03f8f4935

Move disklabel selection logic into DiskLabel.

blivet/formats/disklabel.py
@@ -20,7 +20,11 @@
 # Red Hat Author(s): Dave Lehman <dlehman@redhat.com>
 #
 
+import gi
 import os
+
+gi.require_version("BlockDev", "2.0")
+from gi.repository import BlockDev as blockdev
 
 from ..storage_log import log_exception_info, log_method_call
 import parted
@@ -45,6 +49,7 @@ class DiskLabel(DeviceFormat):
     _type = "disklabel"
     _name = N_("partition table")
     _formattable = True                # can be formatted
+    _default_label_type = None
 
     def __init__(self, **kwargs):
         """
@@ -59,10 +64,9 @@ class DiskLabel(DeviceFormat):
         log_method_call(self, **kwargs)
         DeviceFormat.__init__(self, **kwargs)
 
+        self._label_type = ""
         if not self.exists:
-            self._label_type = kwargs.get("label_type", "msdos")
-        else:
-            self._label_type = ""
+            self._label_type = kwargs.get("label_type") or ""
 
         self._size = Size(0)
 
@@ -151,8 +155,8 @@ class DiskLabel(DeviceFormat):
 
     def fresh_parted_disk(self):
         """ Return a new, empty parted.Disk instance for this device. """
-        log_method_call(self, device=self.device, label_type=self._label_type)
-        return parted.freshDisk(device=self.parted_device, ty=self._label_type)
+        log_method_call(self, device=self.device, label_type=self.label_type)
+        return parted.freshDisk(device=self.parted_device, ty=self.label_type)
 
     @property
     def parted_disk(self):
@@ -173,10 +177,6 @@ class DiskLabel(DeviceFormat):
                     # same as if the device had no label (cause it really
                     # doesn't).
                     raise InvalidDiskLabelError()
-
-                # here's where we correct the ctor-supplied disklabel type for
-                # preexisting disklabels if the passed type was wrong
-                self._label_type = self._parted_disk.type
             else:
                 self._parted_disk = self.fresh_parted_disk()
 
@@ -218,11 +218,70 @@ class DiskLabel(DeviceFormat):
             log.info("DiskLabel.parted_device returning None")
         return self._parted_device
 
+    @classmethod
+    def get_platform_label_types(cls):
+        label_types = ["msdos", "gpt"]
+        if arch.is_pmac():
+            label_types = ["mac"]
+        elif arch.is_efi() and not arch.is_aarch64():
+            label_types = ["gpt"]
+        elif arch.is_s390():
+            label_types = ["msdos"]  # since 'dasd' is only for DASD, it isn't listed here
+
+        return label_types
+
+    @classmethod
+    def set_default_label_type(cls, labeltype):
+        cls._default_label_type = labeltype
+        log.debug("default disklabel has been set to %s", labeltype)
+
+    def _label_type_size_check(self, label_type):
+        if self.parted_device is None:
+            return False
+
+        label = parted.freshDisk(device=self.parted_device, ty=label_type)
+        return self.parted_device.length < label.maxPartitionStartSector
+
+    def _get_best_label_type(self):
+        label_type = self._default_label_type
+        label_types = self.get_platform_label_types()[:]
+        if label_type in label_types:
+            label_types.remove(label_type)
+        if label_type:
+            label_types.insert(0, label_type)
+
+        if arch.is_s390():
+            if blockdev.s390.dasd_is_fba(self.device):
+                # the device is FBA DASD
+                return "msdos"
+            elif self.parted_device.type == parted.DEVICE_DASD:
+                # the device is DASD
+                return "dasd"
+
+        for lt in label_types:
+            if self._label_type_size_check(lt):
+                log.debug("selecting %s disklabel for %s based on size",
+                          label_type, os.path.basename(self.device))
+                label_type = lt
+                break
+
+        return label_type
+
     @property
     def label_type(self):
         """ The disklabel type (eg: 'gpt', 'msdos') """
         if not self.supported:
             return self._label_type
+
+        # For new disklabels, user-specified type overrides built-in logic.
+        # XXX This determines the type we pass to parted.Disk
+        if not self.exists and not self._parted_disk:
+            if self._label_type:
+                lt = self._label_type
+            else:
+                lt = self._get_best_label_type()
+
+            return lt
 
         try:
             lt = self.parted_disk.type

tests/formats_test/disklabel_test.py
@@ -63,3 +63,104 @@ class DiskLabelTestCase(unittest.TestCase):
         # test the old deprecated properties' values
         self.assertEqual(dl.alignment, dl._get_optimal_alignment())
         self.assertEqual(dl.end_alignment, dl.get_end_alignment())
+
+    @patch("blivet.formats.disklabel.arch")
+    def test_platform_label_types(self, arch):
+        disklabel_class = blivet.formats.disklabel.DiskLabel
+
+        arch.is_s390.return_value = False
+        arch.is_efi.return_value = False
+        arch.is_aarch64.return_value = False
+        arch.is_pmac.return_value = False
+
+        self.assertEqual(disklabel_class.get_platform_label_types(), ["msdos", "gpt"])
+
+        arch.is_pmac.return_value = True
+        self.assertEqual(disklabel_class.get_platform_label_types(), ["mac"])
+        arch.is_pmac.return_value = False
+
+        arch.is_efi.return_value = True
+        self.assertEqual(disklabel_class.get_platform_label_types(), ["gpt"])
+        arch.is_efi.return_value = False
+
+        arch.is_s390.return_value = True
+        self.assertEqual(disklabel_class.get_platform_label_types(), ["msdos"])
+        arch.is_s390.return_value = False
+
+    def test_label_type_size_check(self):
+        dl = blivet.formats.disklabel.DiskLabel()
+        dl._parted_disk = mock.Mock()
+        dl._parted_device = mock.Mock()
+
+        with patch("blivet.formats.disklabel.parted") as patched_parted:
+            patched_parted.freshDisk.return_value = mock.Mock(name="parted.Disk", maxPartitionStartSector=10)
+            dl._parted_device.length = 100
+            self.assertEqual(dl._label_type_size_check("foo"), False)
+
+            dl._parted_device.length = 10
+            self.assertEqual(dl._label_type_size_check("foo"), False)
+
+            dl._parted_device.length = 9
+            self.assertEqual(dl._label_type_size_check("foo"), True)
+
+        with patch.object(blivet.formats.disklabel.DiskLabel, "parted_device", new=mock.PropertyMock(return_value=None)):
+            # no parted device -> no passing size check
+            self.assertEqual(dl._label_type_size_check("msdos"), False)
+
+    @patch("blivet.formats.disklabel.arch")
+    def test_best_label_type(self, arch):
+        """
+            1. is always in _disklabel_types
+            2. is the default unless the device is too long for the default
+            3. is msdos for fba dasd on S390
+            4. is dasd for non-fba dasd on S390
+        """
+        dl = blivet.formats.disklabel.DiskLabel()
+        dl._parted_disk = mock.Mock()
+        dl._parted_device = mock.Mock()
+        dl._device = "labeltypefakedev"
+
+        arch.is_s390.return_value = False
+        arch.is_efi.return_value = False
+        arch.is_aarch64.return_value = False
+        arch.is_pmac.return_value = False
+
+        with mock.patch.object(dl, '_label_type_size_check') as size_check:
+            # size check passes for first type ("msdos")
+            size_check.return_value = True
+            self.assertEqual(dl._get_best_label_type(), "msdos")
+
+            # size checks all fail -> label type is None
+            size_check.return_value = False
+            self.assertEqual(dl._get_best_label_type(), None)
+
+            # size check passes on second call -> label type is "gpt" (second in platform list)
+            size_check.side_effect = [False, True]
+            self.assertEqual(dl._get_best_label_type(), "gpt")
+
+        arch.is_pmac.return_value = True
+        with mock.patch.object(dl, '_label_type_size_check') as size_check:
+            size_check.return_value = True
+            self.assertEqual(dl._get_best_label_type(), "mac")
+        arch.is_pmac.return_value = False
+
+        arch.is_efi.return_value = True
+        with mock.patch.object(dl, '_label_type_size_check') as size_check:
+            size_check.return_value = True
+            self.assertEqual(dl._get_best_label_type(), "gpt")
+        arch.is_efi.return_value = False
+
+        arch.is_s390.return_value = True
+        with mock.patch.object(dl, '_label_type_size_check') as size_check:
+            size_check.return_value = True
+            with mock.patch("blivet.formats.disklabel.blockdev.s390") as _s390:
+                _s390.dasd_is_fba.return_value = False
+                self.assertEqual(dl._get_best_label_type(), "msdos")
+
+                _s390.dasd_is_fba.return_value = True
+                self.assertEqual(dl._get_best_label_type(), "msdos")
+
+                _s390.dasd_is_fba.return_value = False
+                dl._parted_device.type = parted.DEVICE_DASD
+                self.assertEqual(dl._get_best_label_type(), "dasd")
+        arch.is_s390.return_value = False

blivet/blivet.py
@@ -412,11 +412,8 @@ class Blivet(object):
         destroy_action = ActionDestroyFormat(disk)
         self.devicetree.actions.add(destroy_action)
 
-        label_type = _platform.best_disklabel_type(disk)
-
         # create a new disklabel on the disk
-        new_label = get_format("disklabel", device=disk.path,
-                               label_type=label_type)
+        new_label = get_format("disklabel", device=disk.path)
         create_action = ActionCreateFormat(disk, fmt=new_label)
         self.devicetree.actions.add(create_action)
 

blivet/platform.py
@@ -31,6 +31,7 @@ import parted
 
 from . import arch
 from .devicelibs import raid
+from .formats import get_device_format_class
 from .flags import flags
 from .partspec import PartSpec
 from .size import Size
@@ -61,7 +62,6 @@ class Platform(object):
     _boot_partition_description = N_("First sector of boot partition")
     _boot_descriptions = {}
 
-    _disklabel_types = []
     _non_linux_format_types = []
 
     def __init__(self):
@@ -74,42 +74,16 @@ class Platform(object):
 
     def update_from_flags(self):
         if flags.gpt:
-            if not self.set_default_disklabel_type("gpt"):
+            disklabel_class = get_device_format_class("disklabel")
+            disklabel_types = disklabel_class.get_platform_label_types()
+            if "gpt" not in disklabel_types:
                 log.warning("GPT is not a supported disklabel on this platform. Using default "
-                            "disklabel %s instead.", self.default_disklabel_type)
+                            "disklabel %s instead.", disklabel_types[0])
+            else:
+                disklabel_class.set_default_label_type("gpt")
 
     def __call__(self):
         return self
-
-    @property
-    def disklabel_types(self):
-        """A list of valid disklabel types for this architecture."""
-        return self._disklabel_types
-
-    @property
-    def default_disklabel_type(self):
-        """The default disklabel type for this architecture."""
-        return self.disklabel_types[0]
-
-    def set_default_disklabel_type(self, disklabel):
-        """Make the disklabel the default
-
-           :param str disklabel: The disklabel type to set as default
-           :returns: True if successful False if disklabel not supported
-
-           If the disklabel is not supported on the platform it will return
-           False and make no change to the disklabel list.
-
-           If it is supported it will move it to the start of the list,
-           making it the default.
-        """
-        if disklabel not in self._disklabel_types:
-            return False
-
-        self._disklabel_types.remove(disklabel)
-        self._disklabel_types.insert(0, disklabel)
-        log.debug("Default disklabel has been set to %s", disklabel)
-        return True
 
     @property
     def boot_stage1_constraint_dict(self):
@@ -122,27 +96,6 @@ class Platform(object):
              "raid_member_types": self._boot_stage1_raid_member_types,
              "descriptions": dict((k, _(v)) for k, v in self._boot_descriptions.items())}
         return d
-
-    def best_disklabel_type(self, device):
-        """The best disklabel type for the specified device."""
-        if flags.testing:
-            return self.default_disklabel_type
-
-        parted_device = parted.Device(path=device.path)
-        label_type = self.default_disklabel_type
-        log.debug("default disklabel type for %s is %s", device.name, label_type)
-
-        # use the first supported type for this platform
-        # that is large enough to address the whole device
-        for lt in self.disklabel_types:
-            l = parted.freshDisk(device=parted_device, ty=lt)
-            if l.maxPartitionStartSector > parted_device.length:
-                label_type = lt
-                log.debug("selecting %s disklabel for %s based on size",
-                          label_type, device.name)
-                break
-
-        return label_type
 
     @property
     def packages(self):
@@ -180,7 +133,6 @@ class X86(Platform):
                           "partition": Platform._boot_partition_description,
                           "mdarray": Platform._boot_raid_description}
 
-    _disklabel_types = ["msdos", "gpt"]
     # XXX hpfs, if reported by blkid/udev, will end up with a type of None
     _non_linux_format_types = ["vfat", "ntfs", "hpfs"]
     _boot_stage1_missing_error = N_("You must include at least one MBR- or "
@@ -204,7 +156,6 @@ class EFI(Platform):
     _boot_descriptions = {"partition": _boot_efi_description,
                           "mdarray": Platform._boot_raid_description}
 
-    _disklabel_types = ["gpt"]
     # XXX hpfs, if reported by blkid/udev, will end up with a type of None
     _non_linux_format_types = ["vfat", "ntfs", "hpfs"]
     _boot_stage1_missing_error = N_("For a UEFI installation, you must include "
@@ -235,7 +186,6 @@ class MacEFI(EFI):
 
 class Aarch64EFI(EFI):
     _non_linux_format_types = ["vfat", "ntfs"]
-    _disklabel_types = ["gpt", "msdos"]
 
 
 class PPC(Platform):
@@ -252,7 +202,6 @@ class IPSeriesPPC(PPC):
     _boot_stage1_max_end = Size("4 GiB")
     _boot_prep_description = N_("PReP Boot Partition")
     _boot_descriptions = {"partition": _boot_prep_description}
-    _disklabel_types = ["msdos", "gpt"]
     _boot_stage1_missing_error = N_("You must include a PReP Boot Partition "
                                     "within the first 4GiB of an MBR- "
                                     "or GPT-formatted disk.")
@@ -267,7 +216,6 @@ class NewWorldPPC(PPC):
     _boot_stage1_format_types = ["appleboot"]
     _boot_apple_description = N_("Apple Bootstrap Partition")
     _boot_descriptions = {"partition": _boot_apple_description}
-    _disklabel_types = ["mac"]
     _non_linux_format_types = ["hfs", "hfs+"]
     _boot_stage1_missing_error = N_("You must include an Apple Bootstrap "
                                     "Partition on an Apple Partition Map-"
@@ -285,7 +233,6 @@ class PS3(PPC):
 
 class S390(Platform):
     _packages = ["s390utils"]
-    _disklabel_types = ["msdos", "dasd"]
     _boot_stage1_device_types = ["disk", "partition"]
     _boot_dasd_description = N_("DASD")
     _boot_mbr_description = N_("Master Boot Record")
@@ -304,21 +251,6 @@ class S390(Platform):
         """Return the default platform-specific partitioning information."""
         return [PartSpec(mountpoint="/boot", size=Size("1GiB"), lv=False)]
 
-    def best_disklabel_type(self, device):
-        """The best disklabel type for the specified device."""
-        if flags.testing:
-            return self.default_disklabel_type
-
-        # the device is FBA DASD
-        if blockdev.s390.dasd_is_fba(device.path):
-            return "msdos"
-        # the device is DASD
-        elif parted.Device(path=device.path).type == parted.DEVICE_DASD:
-            return "dasd"
-
-        # other types of devices
-        return super(S390, self).best_disklabel_type(device)
-
 
 class ARM(Platform):
     _arm_machine = None
@@ -327,7 +259,6 @@ class ARM(Platform):
     _boot_descriptions = {"disk": _boot_mbr_description,
                           "partition": Platform._boot_partition_description}
 
-    _disklabel_types = ["msdos", "gpt"]
     _boot_stage1_missing_error = N_("You must include at least one MBR-formatted "
                                     "disk as an install target.")
 

tests/devices_test/partition_test.py
@@ -38,7 +38,7 @@ class PartitionDeviceTestCase(unittest.TestCase):
     def test_target_size(self):
         with sparsetmpfile("targetsizetest", Size("10 MiB")) as disk_file:
             disk = DiskFile(disk_file)
-            disk.format = get_format("disklabel", device=disk.path)
+            disk.format = get_format("disklabel", device=disk.path, label_type="msdos")
             grain_size = Size(disk.format.alignment.grainSize)
             sector_size = Size(disk.format.parted_device.sectorSize)
             start = int(grain_size)
@@ -183,7 +183,7 @@ class PartitionDeviceTestCase(unittest.TestCase):
     def test_extended_min_size(self):
         with sparsetmpfile("extendedtest", Size("10 MiB")) as disk_file:
             disk = DiskFile(disk_file)
-            disk.format = get_format("disklabel", device=disk.path)
+            disk.format = get_format("disklabel", device=disk.path, label_type="msdos")
             grain_size = Size(disk.format.alignment.grainSize)
             sector_size = Size(disk.format.parted_device.sectorSize)
 

tests/partitioning_test.py
@@ -148,7 +148,7 @@ class PartitioningTestCase(unittest.TestCase):
     def test_add_partition(self):
         with sparsetmpfile("addparttest", Size("50 MiB")) as disk_file:
             disk = DiskFile(disk_file)
-            disk.format = get_format("disklabel", device=disk.path, exists=False)
+            disk.format = get_format("disklabel", device=disk.path, exists=False, label_type="msdos")
 
             free = disk.format.parted_disk.getFreeSpaceRegions()[0]
 
@@ -319,11 +319,11 @@ class PartitioningTestCase(unittest.TestCase):
         self.assertEqual(req2.growth, 0)
         self.assertEqual(req3.growth, 35)
 
-    def test_disk_chunk1(self):
+    def test_msdos_disk_chunk1(self):
         disk_size = Size("100 MiB")
         with sparsetmpfile("chunktest", disk_size) as disk_file:
             disk = DiskFile(disk_file)
-            disk.format = get_format("disklabel", device=disk.path, exists=False)
+            disk.format = get_format("disklabel", device=disk.path, exists=False, label_type="msdos")
 
             p1 = PartitionDevice("p1", size=Size("10 MiB"), grow=True)
             p2 = PartitionDevice("p2", size=Size("30 MiB"), grow=True)
@@ -340,7 +340,9 @@ class PartitioningTestCase(unittest.TestCase):
             requests = [PartitionRequest(p) for p in partitions]
             chunk = DiskChunk(free[0], requests=requests)
 
-            # parted reports a first free sector of 32 for disk files. whatever.
+            # parted reports a first free sector of 32 for msdos on disk files. whatever.
+            # XXX on gpt, the start is increased to 34 and the end is reduced from 204799 to 204766,
+            #     yielding an expected length of 204733
             length_expected = 204768
             self.assertEqual(chunk.length, length_expected)
 
@@ -370,11 +372,11 @@ class PartitioningTestCase(unittest.TestCase):
             self.assertEqual(requests[0].growth, 30712)
             self.assertEqual(requests[1].growth, 92136)
 
-    def test_disk_chunk2(self):
+    def test_msdos_disk_chunk2(self):
         disk_size = Size("100 MiB")
         with sparsetmpfile("chunktest", disk_size) as disk_file:
             disk = DiskFile(disk_file)
-            disk.format = get_format("disklabel", device=disk.path, exists=False)
+            disk.format = get_format("disklabel", device=disk.path, exists=False, label_type="msdos")
 
             p1 = PartitionDevice("p1", size=Size("10 MiB"), grow=True)
             p2 = PartitionDevice("p2", size=Size("30 MiB"), grow=True)

tests/platform_test.py
@@ -1,94 +1,0 @@
-import unittest
-from unittest import mock
-
-from blivet import platform
-
-
-class PlatformTestCase(unittest.TestCase):
-    def test_default_disklabel_type(self):
-        for name in dir(platform):
-            cls = getattr(platform, name)
-            try:
-                if not issubclass(cls, platform.Platform):
-                    # not a platform class instance
-                    continue
-            except TypeError:
-                # not a class
-                continue
-
-            if not cls._disklabel_types:
-                continue
-
-            obj = cls()
-            type_one = obj.__class__._disklabel_types[0]
-            self.assertEqual(obj.default_disklabel_type, type_one)
-            if len(obj._disklabel_types) > 1:
-                new_default = obj.__class__._disklabel_types[-1]
-                obj.set_default_disklabel_type(new_default)
-                self.assertEqual(obj.default_disklabel_type, new_default)
-
-    def test_get_best_disklabel_type(self):
-        def fresh_disk(device, ty):  # pylint: disable=unused-argument
-            """ Return fake parted.Disk w/ maxPartitionStartSector values suitable for testing. """
-            max_start = 1001
-            if ty == "gpt":
-                max_start = 5001
-
-            return mock.Mock(maxPartitionStartSector=max_start)
-
-        for name in dir(platform):
-            cls = getattr(platform, name)
-            try:
-                if not issubclass(cls, platform.Platform):
-                    # not a platform class instance
-                    continue
-            except TypeError:
-                # not a class
-                continue
-
-            if not cls._disklabel_types:
-                continue
-
-            obj = cls()
-
-            """
-                1. is always in _disklabel_types
-                2. is the default unless the device is too long for the default
-                3. is msdos for fba dasd on S390
-                4. is dasd for non-fba dasd on S390
-            """
-            length = 1000
-            blivetdev = mock.Mock()
-            blivetdev.name = "testdev1"
-            parteddev = mock.Mock()
-            parteddev.length = length
-            with mock.patch("blivet.platform.parted") as _parted:
-                _parted.freshDisk.return_value = mock.Mock(maxPartitionStartSector=length + 1)
-                _parted.Device.return_value = parteddev
-                with mock.patch("blivet.platform.blockdev.s390") as _s390:
-                    if name == "S390":
-                        _s390.dasd_is_fba.return_value = False
-                        parteddev.type = platform.parted.DEVICE_DASD
-                        self.assertEqual(obj.best_disklabel_type(blivetdev), "dasd")
-
-                        _s390.dasd_is_fba.return_value = True
-                        self.assertEqual(obj.best_disklabel_type(blivetdev), "msdos")
-
-                        _s390.dasd_is_fba.return_value = False
-                        parteddev.type = platform.parted.DEVICE_SCSI
-
-                    best_label_type = obj.best_disklabel_type(blivetdev)
-
-                self.assertEqual(best_label_type, obj.default_disklabel_type)
-
-                if cls._disklabel_types != ["msdos", "gpt"]:
-                    continue
-
-                # Now just make sure that we suggest gpt for devices longer than the msdos
-                # disklabel maximum.
-                _parted.freshDisk.return_value = mock.Mock()
-                _parted.freshDisk.side_effect = fresh_disk
-                parteddev.length = 4000
-                best_label_type = obj.best_disklabel_type(blivetdev)
-                self.assertEqual(obj.default_disklabel_type, "msdos")
-                self.assertEqual(best_label_type, "gpt")

commit 4c58ccbd1aba4904d8d33ce5699b43bbaaffa9ec

Dasd is a valid label type on s390x (#1538550)
Add the dasd label type to the list of valid label types for s390x.
This bug was introduced in the commit ebd43b6.

Resolves: rhbz#1538550

blivet/formats/disklabel.py
@@ -226,7 +226,7 @@ class DiskLabel(DeviceFormat):
         elif arch.is_efi() and not arch.is_aarch64():
             label_types = ["gpt"]
         elif arch.is_s390():
-            label_types = ["msdos"]  # since 'dasd' is only for DASD, it isn't listed here
+            label_types = ["msdos", "dasd"]
 
         return label_types
 

tests/formats_test/disklabel_test.py
@@ -84,7 +84,7 @@ class DiskLabelTestCase(unittest.TestCase):
         arch.is_efi.return_value = False
 
         arch.is_s390.return_value = True
-        self.assertEqual(disklabel_class.get_platform_label_types(), ["msdos"])
+        self.assertEqual(disklabel_class.get_platform_label_types(), ["msdos", "dasd"])
         arch.is_s390.return_value = False
 
     def test_label_type_size_check(self):