storaged-project/blivet

commit 2a19dd6638068f32643282d298b5126f57e71330

Do not copy ksdata.
Explodes with pykickstart-3.

blivet/blivet.py
@@ -1759,7 +1759,17 @@ class Blivet(object, metaclass=SynchronizedMeta):
 
     def copy(self):
         log.debug("starting Blivet copy")
+
+        # Do not copy ksdata
+        old_data = self.ksdata
+        self.ksdata = None
+
         new = copy.deepcopy(self)
+
+        # Recover ksdata
+        self.ksdata = old_data
+        new.ksdata = old_data
+
         # go through and re-get parted_partitions from the disks since they
         # don't get deep-copied
         hidden_partitions = [d for d in new.devicetree._hidden

commit c9c2e23e6f9618735b1fc13fab654e681c27cfb8

Fix of the failing master
removed changes from 2a19dd663806

blivet/blivet.py
@@ -1182,17 +1182,7 @@ class Blivet(object):
 
     def copy(self):
         log.debug("starting Blivet copy")
-
-        # Do not copy ksdata
-        old_data = self.ksdata
-        self.ksdata = None
-
         new = copy.deepcopy(self)
-
-        # Recover ksdata
-        self.ksdata = old_data
-        new.ksdata = old_data
-
         # go through and re-get parted_partitions from the disks since they
         # don't get deep-copied
         hidden_partitions = [d for d in new.devicetree._hidden