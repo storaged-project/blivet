8a68f7bf5c5df13984587ee4a9e6eeca6236d1d3
David Lehman <dlehman@redhat.com>
2019-03-29 15:42:06 -0400

    Adapt udev test to new package name.

--- tests/udev_test.py
+++ tests/udev_test.py
@@ -2,36 +2,33 @@
 import unittest
 import mock
 
+import blivet3.udev as udev
+
 
 class UdevTest(unittest.TestCase):
 
     def setUp(self):
-        import blivet3.udev
-        self._blivet_os = blivet.udev.os
-        self._blivet_log = blivet.udev.log
-        self._blivet_util = blivet.udev.util
-        blivet.udev.os = mock.Mock()
-        blivet.udev.log = mock.Mock()
-        blivet.udev.util = mock.Mock()
+        self._blivet_os = udev.os
+        self._blivet_log = udev.log
+        self._blivet_util = udev.util
+        udev.os = mock.Mock()
+        udev.log = mock.Mock()
+        udev.util = mock.Mock()
 
     def tearDown(self):
-        import blivet3.udev
-        blivet.udev.log = self._blivet_log
-        blivet.udev.os = self._blivet_os
-        blivet.udev.util = self._blivet_util
+        udev.log = self._blivet_log
+        udev.os = self._blivet_os
+        udev.util = self._blivet_util
 
     def test_udev_get_device(self):
-        import blivet3.udev
-        devices = blivet.udev.global_udev.list_devices(subsystem="block")
+        devices = udev.global_udev.list_devices(subsystem="block")
         for device in devices:
-            self.assertNotEqual(blivet.udev.get_device(device.sys_path), None)
+            self.assertNotEqual(udev.get_device(device.sys_path), None)
 
     def udev_settle_test(self):
-        import blivet3.udev
-        blivet.udev.settle()
-        self.assertTrue(blivet.udev.util.run_program.called)
+        udev.settle()
+        self.assertTrue(udev.util.run_program.called)
 
     def udev_trigger_test(self):
-        import blivet3.udev
-        blivet.udev.trigger()
-        self.assertTrue(blivet.udev.util.run_program.called)
+        udev.trigger()
+        self.assertTrue(udev.util.run_program.called)
