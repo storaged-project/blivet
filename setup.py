5e0192b7881f3c831dcc79f34996fdaec1f13bc2
Vojtech Trefny <vtrefny@redhat.com>
2023-02-15 14:16:17 +0100

    Remove support for Python 2 from spec and Makefile
    Python 2 EOL was 3 years ago so we can stop supporting it now.

--- setup.py
+++ setup.py
@@ -98,7 +98,6 @@ setup(name='blivet',
       classifiers=["Development Status :: 5 - Production/Stable",
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-                   "Programming Language :: Python :: 2",
                    "Programming Language :: Python :: 3",
                    "Operating System :: POSIX :: Linux"]
      )
