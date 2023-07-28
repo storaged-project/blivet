fc2a67a5097e19ed39f81a3f35b8607675046276
Tomas Bzatek <tbzatek@redhat.com>
2023-07-28 17:23:47 +0200

    iscsi: Downgrade default CHAP auth algs to SHA1,MD5
    While the full 'SHA3-256,SHA256,SHA1,MD5' seems to be open-iscsi defaults,
    turned out that legacy tgtd only accepts 'SHA1,MD5'. For some reason
    automatic fallback to other algorithms in the list doesn't really work
    in the downstream libiscsi, returning failure right away.

--- blivet/iscsi.py
+++ blivet/iscsi.py
@@ -278,7 +278,7 @@ class iSCSI(object):
         if extra is None:
             extra = dict()
         extra["node.startup"] = GLib.Variant("s", "automatic")
-        extra["node.session.auth.chap_algs"] = GLib.Variant("s", "SHA3-256,SHA256,SHA1,MD5")
+        extra["node.session.auth.chap_algs"] = GLib.Variant("s", "SHA1,MD5")
 
         args = GLib.Variant("(sisisa{sv})", node_info.conn_info + (extra,))
         self._call_initiator_method("Login", args)
