33a8c69b7812671e8052707d72e681c6b2ab9116
Vojtech Trefny <vtrefny@redhat.com>
2023-12-06 11:47:31 +0100

    tests: Add a simple unit test for the NVMe module

---
+++ tests/unit_tests/nvme_test.py
@@ -0,0 +1,38 @@
+import unittest
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from blivet.nvme import nvme
+
+
+class NVMeModuleTestCase(unittest.TestCase):
+
+    host_nqn = "nqn.2014-08.org.nvmexpress:uuid:01234567-8900-abcd-efff-abcdabcdabcd"
+
+    @patch("blivet.nvme.os")
+    @patch("blivet.nvme.blockdev")
+    def test_nvme_module(self, bd, os):
+        self.assertIsNotNone(nvme)
+        bd.nvme_get_host_nqn.return_value = self.host_nqn
+        bd.nvme_get_host_id.return_value = None  # None = generate from host_nqn
+        os.path.isdir.return_value = False
+
+        # startup
+        with patch.object(nvme, "write") as write:
+            nvme.startup()
+            write.assert_called_once_with("/", overwrite=False)
+
+        self.assertTrue(nvme.started)
+        self.assertEqual(nvme._hostnqn, self.host_nqn)
+        self.assertEqual(nvme._hostid, "01234567-8900-abcd-efff-abcdabcdabcd")
+
+        # write
+        with patch("blivet.nvme.open") as op:
+            nvme.write("/test")
+
+            os.makedirs.assert_called_with("/test/etc/nvme/", 0o755)
+            op.assert_any_call("/test/etc/nvme/hostnqn", "w")
+            op.assert_any_call("/test/etc/nvme/hostid", "w")
