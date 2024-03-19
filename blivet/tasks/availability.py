4e2c9e89bdb41688786a50244d40a3dd71f8ce10
Vojtech Trefny <vtrefny@redhat.com>
2024-03-19 13:20:31 +0100

    Try to start stratisd before checking its availability
    stratisd is neither autostarted after installing the package nor
    DBus activated so we need to try to start it first.

--- blivet/tasks/availability.py
+++ blivet/tasks/availability.py
@@ -26,6 +26,7 @@ from six import add_metaclass
 
 from .. import safe_dbus
 from ..devicelibs.stratis import STRATIS_SERVICE, STRATIS_PATH
+from .. import util
 
 import gi
 gi.require_version("BlockDev", "3.0")
@@ -317,6 +318,11 @@ class DBusMethod(Method):
             :returns: [] if the name of the plugin is loaded
             :rtype: list of str
         """
+        # try to start the service first
+        ret = util.run_program(["systemctl", "start", resource.name])
+        if ret != 0:
+            return ["DBus service %s not available" % resource.name]
+
         try:
             avail = blockdev.utils.dbus_service_available(None, Gio.BusType.SYSTEM, self.dbus_name, self.dbus_path)
             avail = safe_dbus.check_object_available(self.dbus_name, self.dbus_path)
@@ -599,4 +605,4 @@ STRATISPREDICTUSAGE_APP = application("stratis-predict-usage")
 
 # dbus services
 STRATIS_SERVICE_METHOD = DBusMethod(STRATIS_SERVICE, STRATIS_PATH)
-STRATIS_DBUS = dbus_service("stratis", STRATIS_SERVICE_METHOD)
+STRATIS_DBUS = dbus_service("stratisd", STRATIS_SERVICE_METHOD)
