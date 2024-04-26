e0071a76e1effd00fc91ad2ccdf583614eb9b0a4
Vojtech Trefny <vtrefny@redhat.com>
2024-04-26 13:46:56 +0200

    Use longer timeout for Stratis DBus calls
    We've seen some timeouts for some Stratis calls (especially with
    larger disks and encryption) so we need a longer timeouts for the
    calls. This uses 120 seconds which is the same value which stratis
    CLI uses.

--- blivet/devicelibs/stratis.py
+++ blivet/devicelibs/stratis.py
@@ -46,6 +46,8 @@ STRATIS_MANAGER_INTF = STRATIS_SERVICE + ".Manager.r0"
 
 STRATIS_FS_SIZE = Size("1 TiB")
 
+STRATIS_CALL_TIMEOUT = 120 * 1000  # 120 s (used by stratis-cli by default) in ms
+
 
 safe_name_characters = "0-9a-zA-Z._-"
 
@@ -108,7 +110,8 @@ def remove_pool(pool_uuid):
                                                      STRATIS_PATH,
                                                      STRATIS_MANAGER_INTF,
                                                      "DestroyPool",
-                                                     GLib.Variant("(o)", (pool_info.object_path,)))
+                                                     GLib.Variant("(o)", (pool_info.object_path,)),
+                                                     timeout=STRATIS_CALL_TIMEOUT)
     except safe_dbus.DBusCallError as e:
         raise StratisError("Failed to remove stratis pool: %s" % str(e))
     else:
@@ -136,7 +139,8 @@ def remove_filesystem(pool_uuid, fs_uuid):
                                                      pool_info.object_path,
                                                      STRATIS_POOL_INTF,
                                                      "DestroyFilesystems",
-                                                     GLib.Variant("(ao)", ([fs_info.object_path],)))
+                                                     GLib.Variant("(ao)", ([fs_info.object_path],)),
+                                                     timeout=STRATIS_CALL_TIMEOUT)
     except safe_dbus.DBusCallError as e:
         raise StratisError("Failed to remove stratis filesystem: %s" % str(e))
     else:
@@ -223,7 +227,8 @@ def create_pool(name, devices, encrypted, passphrase, key_file, clevis):
                                                         "CreatePool",
                                                         GLib.Variant("(s(bq)as(bs)(b(ss)))", (name, raid_opt,
                                                                                               devices, key_opt,
-                                                                                              clevis_opt)))
+                                                                                              clevis_opt)),
+                                                        timeout=STRATIS_CALL_TIMEOUT)
     except safe_dbus.DBusCallError as e:
         raise StratisError("Failed to create stratis pool: %s" % str(e))
     else:
@@ -255,7 +260,8 @@ def create_filesystem(name, pool_uuid, fs_size=None):
                                                         pool_info.object_path,
                                                         STRATIS_POOL_INTF,
                                                         "CreateFilesystems",
-                                                        GLib.Variant("(a(s(bs)))", ([GLib.Variant("(s(bs))", (name, size_opt))],)))
+                                                        GLib.Variant("(a(s(bs)))", ([GLib.Variant("(s(bs))", (name, size_opt))],)),
+                                                        timeout=STRATIS_CALL_TIMEOUT)
     except safe_dbus.DBusCallError as e:
         raise StratisError("Failed to create stratis filesystem on '%s': %s" % (pool_info.name, str(e)))
     else:
@@ -280,7 +286,8 @@ def add_device(pool_uuid, device):
                                                         pool_info.object_path,
                                                         STRATIS_POOL_INTF,
                                                         "AddDataDevs",
-                                                        GLib.Variant("(as)", ([device],)))
+                                                        GLib.Variant("(as)", ([device],)),
+                                                        timeout=STRATIS_CALL_TIMEOUT)
     except safe_dbus.DBusCallError as e:
         raise StratisError("Failed to create stratis filesystem on '%s': %s" % (pool_info.name, str(e)))
     else:
