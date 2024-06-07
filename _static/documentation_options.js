a3b14e203639ee29df3ae469c286ff6ef8241271
Vojtech Trefny <vtrefny@redhat.com>
2024-06-07 14:08:52 +0200

    Documentation update for 3.10.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,6 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.10.0',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.10.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
