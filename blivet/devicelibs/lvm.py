ee19e665276fd7cd6477da9bee59641b1de1a916
Vojtech Trefny <vtrefny@redhat.com>
2025-06-27 11:28:09 +0200

    Tell LVM DBus to refresh it's internal status during reset
    Unfortunately some users run wipefs <disk> thinking it's enough
    to remove all devices on top of the disk cleanly.
    In cases where the PV is not directly on the disk, LVM DBus
    doesn't get a udev event and doesn't remove the VG and LVs from
    DBus so we think these still exist.
    
    Resolves: RHEL-93967

--- blivet/devicelibs/lvm.py
+++ blivet/devicelibs/lvm.py
@@ -36,6 +36,7 @@ import logging
 log = logging.getLogger("blivet")
 
 from . import raid
+from .. import safe_dbus
 from ..size import Size
 from ..i18n import N_
 from ..flags import flags
@@ -284,3 +285,21 @@ def recommend_thpool_chunk_size(thpool_size):
 
 def is_valid_cache_md_size(md_size):
     return md_size >= LVM_CACHE_MIN_METADATA_SIZE and md_size <= LVM_CACHE_MAX_METADATA_SIZE
+
+
+def lvm_dbusd_refresh():
+    lvm_soname = blockdev.get_plugin_soname(blockdev.Plugin.LVM)
+    if 'dbus' not in lvm_soname:
+        return
+
+    try:
+        rc = safe_dbus.call_sync("com.redhat.lvmdbus1",
+                                 "/com/redhat/lvmdbus1/Manager",
+                                 "com.redhat.lvmdbus1.Manager",
+                                 "Refresh",
+                                 None)
+    except safe_dbus.DBusCallError as e:
+        log.error("Exception occurred when calling LVM DBusD refresh: %s", str(e))
+    else:
+        if rc[0] != 0:
+            log.error("Failed to call LVM DBusD refresh: %s", rc)
