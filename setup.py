53e49fdb38229dafaff472f9b02468695e8b75ce
Vojtech Trefny <vtrefny@redhat.com>
2025-07-16 10:49:51 +0200

    Remove unused custom setuptools.findall method from setup.py
    This was originally added to include all the symlinks in edd_tests
    but the tests tarball is now created separately with git archive.

--- setup.py
+++ setup.py
@@ -1,45 +1,6 @@
 #!/usr/bin/python3
 
-import setuptools
 from setuptools import setup
-from setuptools.command.sdist import sdist
-import subprocess
-import sys
-import os
-
-# this is copied straight from distutils.filelist.findall , but with os.stat()
-# replaced with os.lstat(), so S_ISLNK() can actually tell us something.
-
-
-def findall(dirname=os.curdir):
-    from stat import ST_MODE, S_ISREG, S_ISDIR, S_ISLNK
-
-    file_list = []
-    stack = [dirname]
-    pop = stack.pop
-    push = stack.append
-
-    while stack:
-        dirname = pop()
-        names = os.listdir(dirname)
-
-        for name in names:
-            if dirname != os.curdir:        # avoid the dreaded "./" syndrome
-                fullname = os.path.join(dirname, name)
-            else:
-                fullname = name
-
-            # Avoid excess stat calls -- just one will do, thank you!
-            stat = os.lstat(fullname)
-            mode = stat[ST_MODE]
-            if S_ISREG(mode):
-                file_list.append(fullname)
-            elif S_ISDIR(mode) and not S_ISLNK(mode):
-                push(fullname)
-
-    return file_list
-
-setuptools.findall = findall
 
 
 with open("README.md", "r") as f:
